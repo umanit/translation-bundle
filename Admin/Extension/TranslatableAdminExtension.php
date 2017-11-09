01104d4e40b74d9aa9b060c6883c8c9c874e978c
Arthur Guigand <aguigand@umanit.fr>
2017-11-09 10:54:14 +0100

    Added translations on list fields

--- Admin/Extension/TranslatableAdminExtension.php
+++ Admin/Extension/TranslatableAdminExtension.php
@@ -4,6 +4,7 @@ namespace Umanit\TranslationBundle\Admin\Extension;
 
 use Sonata\AdminBundle\Admin\AbstractAdminExtension;
 use Sonata\AdminBundle\Admin\AdminInterface;
+use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 
 /**
@@ -13,14 +14,57 @@ use Sonata\AdminBundle\Route\RouteCollection;
  */
 class TranslatableAdminExtension extends AbstractAdminExtension
 {
+    /**
+     * @param ListMapper $listMapper
+     */
+    public function configureListFields(ListMapper $listMapper)
+    {
+        if (!$listMapper->has('locale')) {
+            $listMapper->add('locale');
+        }
+
+        if (!$listMapper->has('translations')) {
+            $listMapper->add('translations', 'array', [
+                'template' => '@UmanitTranslation/Admin/CRUD/list_translations.html.twig',
+            ]);
+        }
+
+        if (!$listMapper->has('_action')) {
+            $listMapper->add('_action', null, [
+                'actions' => [
+                    'edit'   => [],
+                    'show'   => [],
+                    'delete' => [],
+                ],
+            ]);
+        }
+
+        // Update the edit template
+        $options         = $listMapper->get('_action')->getOption('actions');
+        $options['edit'] = ['template' => '@UmanitTranslation/Admin/CRUD/list__action_edit.html.twig'];
+        $listMapper->get('_action')->setOption('actions', $options);
+    }
+
+    /**
+     * @inheritdoc.
+     *
+     * @param AdminInterface  $admin
+     * @param RouteCollection $collection
+     */
     public function configureRoutes(AdminInterface $admin, RouteCollection $collection)
     {
         // Add the tranlate route
         $collection->add('translate', $admin->getRouterIdParameter() . '/translate/{newLocale}', [
-            '_controller' => 'UmanitTranslationBundle:TranslatableCRUD:translate'
+            '_controller' => 'UmanitTranslationBundle:TranslatableCRUD:translate',
         ]);
     }
 
+    /**
+     * @inheritdoc
+     *
+     * @param AdminInterface $admin
+     * @param mixed          $object
+     */
     public function preUpdate(AdminInterface $admin, $object)
     {
         // Re-set the locale to make sure the children share the same
